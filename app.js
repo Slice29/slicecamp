fb84895b6299a7536184fc9889524dbd02dee5c5
Slice29 <dan.cristian2906@gmail.com>
2022-09-20 13:04:10 +0300

    Test Schema and insert 1 camp

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path');
 const mongoose = require('mongoose');
+const Campground = require('./models/campground')
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -15,8 +17,10 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req, res) => {
-    res.render('home')
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyard', description: "cheap camping" });
+    await camp.save();
+    res.send(camp);
 })
 
 app.listen(3000, () => {

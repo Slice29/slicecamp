bb06fef1c91610d906e6289763400a346e9f71e5
Slice29 <dan.cristian2906@gmail.com>
2022-09-27 09:28:55 +0300

    Add update functionality

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express')
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground')
-
+const methodOverride = require('method-override')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -18,6 +19,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -43,6 +46,16 @@ app.get('/campgrounds/:id', async (req, res) => {
     res.render('campgrounds/show', { campground })
 })
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground })
+})
+
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground })
+    res.redirect(`/campgrounds/${campground._id}`)
+})
 
 app.listen(3000, () => {
     console.log("Listening on port 3000...")
